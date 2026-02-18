6fa495ddcceb1e8f083e10cedc0ba932897cd7b4
hwoo7449 <hwoo7449@naver.com>
2026-02-18 21:18:17 +0900

    feat: 학교선택을 함수로 빼고 온보딩 화면에서 홈 화면으로 이동하는 기능 구현

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,20 +1,38 @@
+import 'package:bobmoo/models/university.dart';
+import 'package:bobmoo/providers/univ_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
 
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: TextButton(
-        onPressed: () {
-          Navigator.of(context).pushReplacementNamed("/select_school");
-          return;
-        },
+        onPressed: _openSelectSchool,
         child: Center(
           child: Text("시작하기", style: TextStyle(color: Colors.black)),
         ),
       ),
     );
+  }
+
+  Future<void> _openSelectSchool() async {
+    final University? university = await Navigator.of(
+      context,
+    ).pushNamed<University?>("/select_school", arguments: false);
+
+    if (!mounted) return;
+
+    if (university != null) {
+      context.read<UnivProvider>().updateUniversity(university);
+    }
+    Navigator.of(context).pushNamedAndRemoveUntil("/home", (route) => false);
   }
 }
