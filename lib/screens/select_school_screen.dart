186e9bb6663791e6460c0480a60d4e212425b184
hwoo7449 <hwoo7449@naver.com>
2026-02-21 03:27:27 +0900

    feat: 학교 선택/취소 기능 추가

--- lib/screens/select_school_screen.dart
+++ lib/screens/select_school_screen.dart
@@ -21,6 +21,8 @@ class SelectSchoolScreen extends StatefulWidget {
 }
 
 class _SelectSchoolScreenState extends State<SelectSchoolScreen> {
+  University? _selectedUniv;
+
   PreferredSizeWidget _buildAppBar() {
     return AppBar(
       // Appbar의 기본 여백 제거
@@ -59,8 +61,15 @@ class _SelectSchoolScreenState extends State<SelectSchoolScreen> {
               _buildSearchResult(univs),
               SizedBox(height: 27.h),
 
-              PrimaryButton(text: "선택완료", onTap: () {}),
-              SizedBox(height: 27.h),
+              if (_selectedUniv != null) ...[
+                PrimaryButton(
+                  text: "선택완료",
+                  onTap: () {
+                    Navigator.of(context).pop(_selectedUniv);
+                  },
+                ),
+                SizedBox(height: 27.h),
+              ],
             ],
           ),
         ),
@@ -100,12 +109,20 @@ class _SelectSchoolScreenState extends State<SelectSchoolScreen> {
 
                   return ListTile(
                     minTileHeight: 58.h,
+                    trailing: _selectedUniv == university
+                        ? Icon(Icons.check, size: 25.h)
+                        : null,
+                    iconColor: AppColors.colorGray3,
                     title: Text(
                       university.name,
                       style: AppTypography.search.b17,
                     ),
                     onTap: () {
-                      Navigator.of(context).pop(university);
+                      setState(() {
+                        _selectedUniv = _selectedUniv == university
+                            ? null
+                            : university;
+                      });
                     },
                   );
                 },
