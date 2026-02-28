def0061b0341b4ea2d8db4dde822d532b1cec79d
hwoo7449 <hwoo7449@naver.com>
2026-03-01 00:11:53 +0900

    chore: 사용하지 않는 import 삭제

--- lib/providers/search_provider.dart
+++ lib/providers/search_provider.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:bobmoo/models/university.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
